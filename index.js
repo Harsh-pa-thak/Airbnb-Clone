0f79c2f17b0b86a882a047aabb67a3de025ae207
Harsh Pathak <harsh9934530656pathak@gmail.com>
2026-02-03 21:31:21 +0530

    ejs mate updated

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const ejsMate = require("ejs-mate");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
 app.use(express.json());
-app.set("view engine", "ejs");
-app.set('views',path.join(__dirname,'/views'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs", ejsMate);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
@@ -18,7 +23,6 @@ app.use((req, res, next) => {
   );
   next();
 });
-
 
 main()
   .then(() => {
@@ -36,81 +40,91 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-app.get('/listings',(req,res)=>{
-  Listing.find({}).then((list)=>{
-  res.render('listings',{listings:list});
-  console.log(list);
-  }
-  ).catch((err)=>{
-    console.log(err);
-  });
-  
-});
-app.get('/listing-detail/:id',(req,res)=>{
-  const id = req.params.id;
-  Listing.findById(id).then((listing)=>{
-    res.render('listing-detail',{listing});
-  }).catch((err)=>{
-    console.log(err);
-  });
+app.get("/listings", (req, res) => {
+  Listing.find({})
+    .then((list) => {
+      res.render("listings/listings", { listings: list });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.get('/newListing',(req,res)=>{
-  res.render('newListing');
+app.get("/listing-detail/:id", (req, res) => {
+  const id = req.params.id;
+
+  Listing.findById(id)
+    .then((listing) => {
+      res.render("listings/listing-detail", { listing });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-app.post('/listings',async (req,res)=>{
-  const {title,description,price,location,country,imageUrl} = req.body;
+
+app.get("/newListing", (req, res) => {
+  res.render("listings/newListing");
+});
+
+app.post("/listings", async (req, res) => {
+  const {
+    title,
+    description,
+    price,
+    location,
+    country,
+    imageUrl,
+  } = req.body;
+
   const newListing = new Listing({
     title: title,
     description: description,
     image: imageUrl,
     price: price,
     location: location,
-    country: country
+    country: country,
   });
-  
+
   await newListing.save();
-  res.redirect('/listings');
+  res.redirect("/listings");
 });
-app.get("/editListings/:id",async(req,res)=>{
+
+app.get("/editListings/:id", async (req, res) => {
   const id = req.params.id;
   const listing = await Listing.findById(id);
-  res.render('editListing',{listing});
+
+  res.render("listings/editListing", { listing });
 });
+
 app.put("/listings/:id", async (req, res) => {
   const id = req.params.id;
-  const {title,description,price,location,country,imageUrl} = req.body;
+  const {
+    title,
+    description,
+    price,
+    location,
+    country,
+    imageUrl,
+  } = req.body;
 
-  await Listing.findByIdAndUpdate(id,{
+  await Listing.findByIdAndUpdate(id, {
     title,
     description,
     image: imageUrl,
     price,
     location,
-    country
+    country,
   });
 
-  res.redirect('/listings');
+  res.redirect("/listings");
 });
+
 app.delete("/listings/:id", async (req, res) => {
   const id = req.params.id;
+
   await Listing.findByIdAndDelete(id);
-  res.redirect('/listings');
+  res.redirect("/listings");
 });
-
-// app.get("/testListing", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "My New Villa",
-//     description: "By the beach",
-//     price: 1200,
-//     location: "Calangute, Goa",
-//     country: "India",
-//   });
-
-//   await sampleListing.save();
-//   console.log("sample was saved");
-//   res.send("successful testing");
-// });
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
