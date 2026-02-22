7a70d1bf06a6e52d87e6387d57169d6964585d53
Harsh Pathak <harphy45@gmail.com>
2026-02-22 16:58:46 +0530

    sign up implemnted

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapasync.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup");
 });
 
-router.post("/signup",async (req, res) => {
-    let { username, email, password } = req.body;
-    const user = new User({ username, email });
-    await User.register(user, password);
-    req.flash("success", "Welcome to AirBnb");
-    res.redirect("/listings");
-});
+router.post("/signup",wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const user = new User({ username, email });
+        await User.register(user, password);
+        req.flash("success", "Welcome to AirBnb");
+        res.redirect("/listings");
+    }catch (e) {
+        req.flash("error", e.message);
+        return res.redirect("/signup");
+    }
+}));
 
 module.exports = router;
