bb122a9f7e3c908785b654223ed35766ffb55565
Harsh Pathak <harphy45@gmail.com>
2026-02-22 17:19:18 +0530

    logout

--- routes/user.js
+++ routes/user.js
@@ -31,5 +31,15 @@ router.post("/login",passport.authenticate('local',{failureFlash:true , failureR
     res.redirect("/listings");
 }));
 
+router.get("/logout", (req, res,next) => {
+    req.logout((err) => {
+        if(err){
+            return next(err);
+        }
+        req.flash("success", "You have been logged out");
+        res.redirect("/listings");
+    });
+});
+
 
 module.exports = router;
