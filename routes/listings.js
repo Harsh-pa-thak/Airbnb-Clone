97ffd153ebf95ce5fc67f825c4c28357707d9d8d
Harsh Pathak <harphy45@gmail.com>
2026-02-27 22:46:09 +0530

    changed

--- routes/listings.js
+++ routes/listings.js
@@ -3,21 +3,44 @@ const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapasync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner , validate } = require("../middelware.js");
-const listingController = require("../controllers/listings.js");
 
-router.get("/", wrapAsync(
-  listingController.index
-));
+router.get("/", wrapAsync(async (req, res) => {
+  const listings = await Listing.find({});
+  res.render("listings/listings", { listings});
+}));
+router.get("/newListings", isLoggedIn, (req, res) => {
+  res.render("listings/newListing");
+});
 
-router.get("/newListings", isLoggedIn ,listingController.renderNewForm);
+router.get("/:id", wrapAsync(async (req, res) => {
+  const id = req.params.id;
+  const listing = await Listing.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("owner");
+  if(!listing){
+    req.flash("error", "Cannot find the listing");
+    return res.redirect("/listings");
+  }
+  res.render("listings/listing-detail", { listing });
+}));
 
-router.get("/:id", wrapAsync(listingController.createListing));
+router.post("/", isLoggedIn, validate, wrapAsync(async (req, res,next) => {
+  const newListing = new Listing(req.body);
+  newListing.owner = req.user._id; 
+  await newListing.save();
+  req.flash("success", "Successfully made a new listing");
+  res.redirect("/listings");
+}));
 
-router.post("/", isLoggedIn, validate, wrapAsync(
-  listingController.newlisting
-));
-
-router.get("/:id/edit",isLoggedIn, wrapAsync(listingController.editform));
+router.get("/:id/edit",isLoggedIn, wrapAsync(async (req, res) => {
+  const id = req.params.id;
+  const listing = await Listing.findById(id);
+  if(!listing){
+    req.flash("error", "Cannot find the listing");
+    return res.redirect("/listings");
+  }
+  res.render("listings/editListing", { listing });
+}));
 
 router.put("/:id",isLoggedIn,isOwner,validate, wrapAsync(async (req, res) => {
   const id = req.params.id;
