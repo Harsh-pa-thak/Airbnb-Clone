60fc87671bbbd6d9efcb9045160d16518b701969
Harsh Pathak <harphy45@gmail.com>
2026-02-28 00:43:28 +0530

    changed

--- routes/listings.js
+++ routes/listings.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapasync.js");
-const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner , validate } = require("../middelware.js");
 const controllers = require("../controllers/listings.js");
 
