8dcd29a558df0fa72397f9025c1fe80ee1fc57d7
Harsh Pathak <harphy45@gmail.com>
2026-02-28 00:51:25 +0530

    changed users

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,22 @@
 module.exports.signupGet = (req, res) => {
     res.render("users/signup");
 };
+
+module.exports.signupPost=async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const user = new User({ username, email });
+        await User.register(user, password);
+        req.login(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+        req.flash("success", "Welcome to AirBnb");
+        res.redirect("/listings");
+        });
+        
+    }catch (e) {
+        req.flash("error", e.message);
+        return res.redirect("/signup");
+    }
+};
