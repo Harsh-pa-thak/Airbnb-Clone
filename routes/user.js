ed7ee3f54f14b6986eaa68cf66acf0c33f230a60
Harsh Pathak <harphy45@gmail.com>
2026-02-28 00:49:07 +0530

    changed users

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapasync.js");
-const { route } = require("./user");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middelware.js");
 
