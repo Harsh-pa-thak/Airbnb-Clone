1e804a5ca173e996a9e83c65d0756b16168ce211
Harsh Pathak <harphy45@gmail.com>
2026-03-10 22:55:47 +0530

    listing model chanegd

--- routes/listings.js
+++ routes/listings.js
@@ -9,7 +9,7 @@ const upload = multer({ storage })
 
 router.route("/")
 .get( wrapAsync(controllers.index))
-.post(isLoggedIn, upload.single('image'), validate, wrapAsync(controllers.createListing));
+.post(isLoggedIn, validate, upload.single('image'), wrapAsync(controllers.createListing));
 
 
 router.get("/newListings", isLoggedIn, controllers.renderNewForm);
