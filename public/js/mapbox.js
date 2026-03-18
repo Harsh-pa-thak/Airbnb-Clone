fcc3bcde6d983d10b94e9380abd595d6eb15dc38
Harsh Pathak <harphy45@gmail.com>
2026-03-19 00:08:38 +0530

    map cordinate conversion

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -5,3 +5,13 @@ const map = new mapboxgl.Map({
     center: [80.2705, 13.0843],
     zoom: 9 
 });
+
+async function getCoordinates() {
+  const url = `https://api.mapbox.com/search/geocode/v6/forward?q=${}&limit=1&access_token=${maptoken}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const coordinates = data.features[0].geometry.coordinates;
+  return coordinates;
+}
+
+getCoordinates().then(coords => console.log(coords));
