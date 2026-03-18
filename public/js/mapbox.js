bf0d86d1d238553992dbe2a7e53db68af59e5980
Harsh Pathak <harphy45@gmail.com>
2026-03-19 00:21:20 +0530

    map for each loaction

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,19 +1,23 @@
-
 mapboxgl.accessToken = maptoken;
 
 async function getCoordinates() {
   const url = `https://api.mapbox.com/search/geocode/v6/forward?q=${l}&limit=1&access_token=${maptoken}`;
+
   const response = await fetch(url);
   const data = await response.json();
-  const coordinates = data.features[0].geometry.coordinates;
-  return coordinates;
+
+  return data.features[0].geometry.coordinates;
 }
-let coords = getCoordinates().then(coords => {
-    const map = new mapboxgl.Map({
-        container: 'map', 
-        center: [coords[1], coords[0]],
-        zoom: 9 
-    });
+
+getCoordinates().then(coords => {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: coords,   // already [lng, lat]
+    zoom: 9
+  });
+
+  new mapboxgl.Marker()
+    .setLngLat(coords)
+    .addTo(map);
 });
-
-
