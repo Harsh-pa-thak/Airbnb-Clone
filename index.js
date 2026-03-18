4a139072397615920fc4c3f8a3b607d8d49eccd8
Harsh Pathak <harphy45@gmail.com>
2026-03-18 23:58:03 +0530

    map fix

--- index.js
+++ index.js
@@ -17,12 +17,20 @@ const User = require("./models/user.js");
 const userRoutes = require("./routes/user.js");
 const fetch = require("node-fetch");
 
-// Legacy packages still read util.isArray; point it to the modern API to avoid DEP0044.
-util.isArray = Array.isArray;
-const city = "patna";
+const fetch = require("node-fetch");
 
-const url = `https://api.mapbox.com/search/geocode/v6/forward?q=${city}&access_token=${process.env.MAPBOX_TOKEN}`;
+async function getCoordinates(city) {
+  const url = `https://api.mapbox.com/search/geocode/v6/forward?q=${city}&limit=1&access_token=${process.env.MAPBOX_TOKEN}`;
 
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const coordinates = data.features[0].geometry.coordinates;
+
+  return coordinates;
+}
+
+getCoordinates("Patna").then(coords => console.log(coords));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
